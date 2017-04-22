fd68f9330bc223c1e9445bcd3e341bb64d87c5f3
Thomas Stang <stang.tk@gmail.com>
2017-04-21 18:00:45 -0700

    Add campuses and admins migrations

--- migrations/20170420213543_campuses.js
+++ migrations/20170420213543_campuses.js
@@ -1,8 +1,7 @@
+exports.up = (knex, Promise) => knex.schema.createTable('campuses', (table) => {
+  table.increments('id').primary();
+  table.string('location').notNullable().unique();
+  table.timestamps(true, true);
+});
 
-exports.up = function(knex, Promise) {
-  
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = (knex, Promise) => knex.schema.dropTable('campuses');
--- migrations/20170420213630_admins.js
+++ migrations/20170420213630_admins.js
@@ -1,8 +1,12 @@
+exports.up = (knex, Promise) => knex.schema.createTable('admins', (table) => {
+  table.increments('id').primary();
+  table.string('username', 'char(20)').notNullable();
+  table.string('first_name', 'char(20)').notNullable();
+  table.string('last_name', 'char(20)').notNullable();
+  table.string('email', 'char(65)').notNullable();
+  table.string('gravatar_url', 'char(65)').notNullable();
+  table.integer('campus_id').notNullable().references('id').inTable('campuses');
+  table.timestamps(true, true);
+});
 
-exports.up = function(knex, Promise) {
-  
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = (knex, Promise) => knex.schema.dropTable('admins');
