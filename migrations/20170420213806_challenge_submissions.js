a52ad0c15a832f0a3a82b0845662fa1115a3cedf
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-04-21 18:53:58 -0700

    Add logic for challenge submissions and reward request migrations

--- migrations/20170420213806_challenge_submissions.js
+++ migrations/20170420213806_challenge_submissions.js
@@ -1,8 +1,41 @@
+exports.up = (knex, Promise) => knex.schema.createTable('challenge_submissions', (table) => {
+  table.increments('id')
+  .primary();
 
-exports.up = function(knex, Promise) {
-  
-};
+  table.integer('student_id')
+  .notNullable()
+  .references('id')
+  .inTable('students')
+  .onDelete('cascade');
 
-exports.down = function(knex, Promise) {
-  
-};
+  table.integer('challenges_id')
+  .notNullable()
+  .references('id')
+  .inTable('challenges');
+
+  table.string('submission_message', 'char(512)');
+
+  table.string('evaluation_message', 'char(512)');
+
+  table.string('submission_status', 'char(65)');
+
+  table.string('submission_attachment_1', 'char(65)');
+
+  table.string('submission_attachment_2', 'char(65)');
+
+  table.string('submission_attachment_3', 'char(65)');
+
+  table.string('submission_image_link_1', 'char(65)');
+
+  table.string('submission_image_link_2', 'char(65)');
+
+  table.string('submission_image_link_3', 'char(65)');
+
+  table.timestamp('updated_at')
+  .notNullable()
+  .defaultTo(knex.fn.now());
+
+  table.timestamps(true, true);
+});
+
+exports.down = (knex, Promise) => knex.schema.dropTable('challenge_submissions');
--- migrations/20170420213822_reward_requests.js
+++ migrations/20170420213822_reward_requests.js
@@ -1,8 +1,31 @@
+exports.up = (knex, Promise) => knex.schema.createTable('reward_requests', (table) => {
+  table.increments('id')
+  .primary();
 
-exports.up = function(knex, Promise) {
-  
-};
+  table.string('request_status', 'char(65)')
+  .notNullable();
 
-exports.down = function(knex, Promise) {
-  
-};
+  table.integer('student_id')
+  .notNullable()
+  .references('id')
+  .inTable('students')
+  .onDelete('cascade');
+
+  table.integer('rewards_id')
+  .notNullable()
+  .references('id')
+  .inTable('rewards');
+
+  table.timestamp('updated_at')
+  .notNullable()
+  .defaultTo(knex.fn.now());
+
+  table.string('notes', 'char(256)');
+
+  table.boolean('reward_used')
+  .notNullable();
+
+  table.timestamps(true, true);
+});
+
+exports.down = (knex, Promise) => knex.schema.dropTable('challenge_submissions');
