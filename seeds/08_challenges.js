1bd04806c14f3b9aed1e9ccb373a0a4857e20f6e
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-04-21 23:15:45 -0700

    Add challenges seed data

--- seeds/08_challenges.js
+++ seeds/08_challenges.js
@@ -1,13 +1,47 @@
-
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = (knex, Promise) => {
+  knex('challenges').del()
+  .then(() =>
+    knex('challenges').insert([
+      {
+        id: 1,
+        name: 'Office Hours',
+        point_cost: 25,
+        description: 'Hold office hours during lab time for a junior cohort(min. 1 hour)',
+        campus_id: 1,
+        category_id: 2,
+      },
+      {
+        id: 2,
+        name: 'Front-end side project',
+        point_cost: 75,
+        description: 'Develop a side project using curriculum technology',
+        campus_id: 1,
+        category_id: 1,
+      },
+      {
+        id: 3,
+        name: 'Backend side project',
+        point_cost: 75,
+        description: 'Develop a side project using curriculum technology',
+        campus_id: 1,
+        category_id: 1,
+      },
+      {
+        id: 4,
+        name: 'Fullstack side project',
+        point_cost: 200,
+        description: 'Develop a side project using curriculum technology',
+        campus_id: 1,
+        category_id: 1,
+      },
+      {
+        id: 5,
+        name: 'Publish article',
+        point_cost: 75,
+        description: 'Publish an original article on Medium with a minimum 750 word length',
+        campus_id: 1,
+        category_id: 3,
+      },
+    ]),
+  );
 };
