d5d7f8dabb6aadaed6f2a1cd2880442755979d84
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-04-23 15:57:55 -0700

    Delete campuses route works

--- lvlup-backend/routes/campuses.js
+++ lvlup-backend/routes/campuses.js
@@ -5,22 +5,45 @@ const Campus = require('../models/campus');
 
 const router = express.Router();
 
-router.get('/campuses/', (req, res) => {
-  Campuses.forge()
-  .fetch()
-  .then((campuses) => {
-    // console.log(campuses)
-    // delete campuses.id;
-    // delete campuses.created_at;
-    // delete campuses.updated_at;
-    console.log(campuses.toJSON);
-    res.status(200).json(campuses);
+router.route('/campuses/')
+  .get((req, res) => {
+    Campuses.forge()
+    .fetch()
+    .then((campuses) => {
+      res.json({ error: false, data: campuses.toJSON() });
+    })
+    .catch((err) => {
+      res.status(500).json({error: true, data: { message: err.message } });
+    });
   })
-  .catch(err => console.error(err))
-})
 
-router.post('/campuses/', (req, res) => {
-
-})
+  // .post((req, res) => {
+  //   Campus.forge({
+  //     location: req.body.location,
+  //   })
+  //   .save()
+  //   .then((campus) => {
+  //     res.status(200).json(campus);
+  //   })
+  //   .catch(err => console.error(err))
+  // });
+  router.route('/campuses/:campus_id')
+    .delete((req, res) => {
+      console.log('here');
+      Campus.forge()
+      .fetch({ require: true})
+      .then((campus) => {
+        campus.destroy()
+        .then(() => {
+          res.json({ error: true, data: { message: 'User successfully deleted' } })
+        })
+        .catch((err) => {
+          res.status(500).json({ error: true, data: { message: err.message } });
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: true, data: { message: err.message } });
+      });
+    });
 
 module.exports = router;
