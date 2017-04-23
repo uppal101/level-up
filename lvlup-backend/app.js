8aa8c570e92d230c960866a56b99f4fbdb7416e2
Daniel Gardner <daniel.marc.gardner@gmail.com>
2017-04-22 18:14:40 -0700

    Refactor to move OAuth out of app.js

--- lvlup-backend/app.js
+++ lvlup-backend/app.js
@@ -5,24 +5,20 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const session = require('express-session');
 const GitHubStrategy = require('passport-github2').Strategy;
+const students = require('./routes/students');
+const login = require('./routes/login');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 app.use(bodyParser.json());
 app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// const path = require('path');
-//
-// app.use(express.static(path.join('public')));
-
-const students = require('./routes/students');
+app.use(login);
 app.use(students);
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-
-console.log('hello');
 
 // passport.serializeUser(function (user, done) {
 //   done(null, user);
@@ -33,24 +29,24 @@ console.log('hello');
 // });
 
 
-passport.use(new GitHubStrategy({
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: `${process.env.HOST}/auth/github/callback`,
-},
-  (accessToken, refreshToken, profile, done) => {
-    console.log('profile:', profile);
-  }));
-
-app.get('/auth/github',
-  passport.authenticate('github', { scope: ['user:email'] }));
-
-app.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  (req, res) => {
-    // Successful authentication, redirect home.
-    res.json(res);
-  });
+// passport.use(new GitHubStrategy({
+//   clientID: process.env.GITHUB_CLIENT_ID,
+//   clientSecret: process.env.GITHUB_CLIENT_SECRET,
+//   callbackURL: `${process.env.HOST}/auth/github/callback`,
+// },
+//   (accessToken, refreshToken, profile, done) => {
+//     console.log('profile:', profile);
+//   }));
+//
+// app.get('/auth/github',
+//   passport.authenticate('github', { scope: ['user:email'] }));
+//
+// app.get('/auth/github/callback',
+//   passport.authenticate('github', { failureRedirect: '/login' }),
+//   (req, res) => {
+//     // Successful authentication, redirect home.
+//     res.json(res);
+//   });
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`);
--- lvlup-backend/routes/login.js
+++ lvlup-backend/routes/login.js
@@ -1,2 +1,33 @@
 const express = require('express');
+const app = express();
 const router = express.Router();
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: `${process.env.HOST}/auth/github/callback`,
+},
+  (accessToken, refreshToken, profile, done) => {
+    console.log('profile:', profile);
+  }));
+
+router.get('/auth/github',
+  passport.authenticate('github', { scope: ['user:email'] }));
+
+router.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.json(res);
+  });
+
+module.exports = router;
