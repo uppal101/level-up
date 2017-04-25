b4bb1a84e19311ff3ef14fc0bee256f4e41ac66b
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-04-24 21:48:33 -0700

    Add skeleton for admin routes and tests

---
+++ lvlup-backend/routes/admins.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const knex = require('../knex.js');
+const Admins = require('../collections/admins');
+const Admin = require('../models/admin');
+
+const router = express.Router();
+
+module.exports = router;
