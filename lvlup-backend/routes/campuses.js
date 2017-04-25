503dbe803c9a1fa30d60473a09ef8d7c5629d7f6
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-04-24 19:46:08 -0700

    Edit linting errors

--- lvlup-backend/routes/campuses.js
+++ lvlup-backend/routes/campuses.js
@@ -6,51 +6,51 @@ const Campus = require('../models/campus');
 const router = express.Router();
 
 router.route('/campuses/')
-  .get((req, res) => {
-    Campuses.forge()
-    .fetch()
-    .then((campuses) => {
-      const campusesResponse = JSON.parse(JSON.stringify(campuses));
-      let locations = campusesResponse.map((ele) => {
-        delete ele.id;
-        delete ele.updated_at;
-        delete ele.created_at;
-        return ele;
-      });
-      res.json({ locations });
+.get((req, res) => {
+  Campuses.forge()
+  .fetch()
+  .then((campuses) => {
+    const campusesResponse = JSON.parse(JSON.stringify(campuses));
+    const locations = campusesResponse.map((ele) => {
+      delete ele.id;
+      delete ele.updated_at;
+      delete ele.created_at;
+      return ele;
+    });
+    res.json({ locations });
+  })
+  .catch((err) => {
+    res.status(500).json({ error: true, data: { message: err.message } });
+  });
+})
+
+.post((req, res) => {
+  Campus.forge({
+    location: req.body.location,
+  })
+  .save()
+  .then((campus) => {
+    res.status(200).json(campus);
+  })
+  .catch(err => console.error(err));
+});
+
+router.route('/campuses/:campus_id')
+.delete((req, res) => {
+  Campus.forge()
+  .fetch({ require: true })
+  .then((campus) => {
+    campus.destroy()
+    .then(() => {
+      res.json({ message: 'Campus successfully deleted' });
     })
     .catch((err) => {
       res.status(500).json({ error: true, data: { message: err.message } });
     });
   })
-
-  .post((req, res) => {
-    Campus.forge({
-      location: req.body.location,
-    })
-    .save()
-    .then((campus) => {
-      res.status(200).json(campus);
-    })
-    .catch(err => console.error(err))
+  .catch((err) => {
+    res.status(500).json({ error: true, data: { message: err.message } });
   });
-
-  router.route('/campuses/:campus_id')
-    .delete((req, res) => {
-      Campus.forge()
-      .fetch({ require: true})
-      .then((campus) => {
-        campus.destroy()
-        .then(() => {
-          res.json({ message: 'Campus successfully deleted' })
-        })
-        .catch((err) => {
-          res.status(500).json({ error: true, data: { message: err.message } });
-        });
-      })
-      .catch((err) => {
-        res.status(500).json({ error: true, data: { message: err.message } });
-      });
-    });
+});
 
 module.exports = router;
--- lvlup-backend/routes/cohorts.js
+++ lvlup-backend/routes/cohorts.js
@@ -6,54 +6,48 @@ const Cohort = require('../models/cohort');
 const router = express.Router();
 
 router.route('/cohorts/')
-  .get((req, res) => {
-    Cohorts.forge()
-    .fetch()
-    .then((cohorts) => {
-      const cohortsResponse = JSON.parse(JSON.stringify(cohorts))
-      let allCohorts = cohortsResponse.map((ele) => {
-        delete ele.id;
-        delete ele.updated_at;
-        delete ele.created_at;
-        return ele;
-      })
-      res.json({ allCohorts });
-
-    })
-    .catch((err) => {
-      res.status(500).json({ error: true, data: { message: err.message } });
+.get((req, res) => {
+  Cohorts.forge()
+  .fetch()
+  .then((cohorts) => {
+    const cohortsResponse = JSON.parse(JSON.stringify(cohorts));
+    const allCohorts = cohortsResponse.map((ele) => {
+      delete ele.id;
+      delete ele.updated_at;
+      delete ele.created_at;
+      return ele;
     });
+    res.json({ allCohorts });
   })
-
-  .post((req, res) => {
-    Cohort.forge({
-      name: req.body.name,
-      type: req.body.type,
-      q1_start_date: req.body.q1_start_date,
-      q2_start_date: req.body.q2_start_date,
-      q3_start_date: req.body.q3_start_date,
-      q4_start_date: req.body.q4_start_date,
-      graduation_date: req.body.graduation_date,
-      campus_id: req.body.campus_id,
-    })
-    .save()
-    .then((cohort) => {
-      res.status(200).json(cohort);
-    })
-    .catch(err => console.error(err))
+  .catch((err) => {
+    res.status(500).json({ error: true, data: { message: err.message } });
   });
+})
+
+.post((req, res) => {
+  Cohort.forge({
+    name: req.body.name,
+    type: req.body.type,
+    q1_start_date: req.body.q1_start_date,
+    q2_start_date: req.body.q2_start_date,
+    q3_start_date: req.body.q3_start_date,
+    q4_start_date: req.body.q4_start_date,
+    graduation_date: req.body.graduation_date,
+    campus_id: req.body.campus_id,
+  })
+  .save()
+  .then((cohort) => {
+    res.status(200).json(cohort);
+  })
+  .catch(err => console.error(err));
+});
 
 router.route('/cohorts/:cohort_id')
   .get((req, res) => {
-    Cohort.forge({ id: req.params.id })
+    Cohort.forge({ id: req.params.cohort_id })
     .fetch()
     .then((cohort) => {
-      // if (!cohort) {
-      //   res.status(404).json({ error: true, data: {} })
-      // }
-      // else {
-        res.status(200).json(cohort);
-      // }
+      res.status(200).json(cohort);
     })
     .catch((err) => {
       res.status(500).json({ error: true, data: { message: err.message } });
@@ -61,15 +55,31 @@ router.route('/cohorts/:cohort_id')
   })
   .delete((req, res) => {
     Cohort.forge()
-    .fetch({ require: true})
+    .fetch({ require: true })
     .then((cohort) => {
       cohort.destroy()
       .then(() => {
-        res.json({ message: 'Cohort successfully deleted' })
+        res.json({ message: 'Cohort successfully deleted' });
       })
       .catch((err) => {
         res.status(500).json({ error: true, data: { message: err.message } });
       });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: true, data: { message: err.message } });
+    });
+  });
+
+router.route('/cohorts/campuses/:campus_id')
+  .get((req, res) => {
+    Cohort.forge({ campus_id: req.params.campus_id })
+    .fetch()
+    .then((cohorts) => {
+      const cohortsResponse = JSON.parse(JSON.stringify(cohorts));
+      delete cohortsResponse.id;
+      delete cohortsResponse.created_at;
+      delete cohortsResponse.updated_at;
+      res.json({ cohortsResponse });
     })
     .catch((err) => {
       res.status(500).json({ error: true, data: { message: err.message } });
