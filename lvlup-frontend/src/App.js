f1621fe94ef67efe5ad063d7fb18003841e739cb
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-04-26 13:25:42 -0700

    Render navbar on admin log-in view

--- lvlup-frontend/src/App.js
+++ lvlup-frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './components/homepage-view/home';
 import StudentHome from './components/student-main-view/student-main';
 // import SignUpForm from './components/admin-signup/signup-page.js';
+import AdminLogin from './components/admin-login/login';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -15,6 +16,7 @@ export default class App extends Component {
         <div>
           <Route exact path="/" component={Home} />
           <Route exact path="/dashboard/student" component={StudentHome} />
+          <Route exact path="/admin-login" component={AdminLogin} />
         </div>
       </Router>
     );
---
+++ lvlup-frontend/src/components/admin-login/login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Navbar from './navbar.js';
+
+class AdminLogin extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default AdminLogin;
