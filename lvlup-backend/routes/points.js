774af1e4c70db8fd09bd51cd990a067059e72fb2
Thomas Stang <stang.tk@gmail.com>
2017-04-27 14:30:34 -0700

    Complete functionality of points algorithm, add get points by student ID route

--- lvlup-backend/routes/points.js
+++ lvlup-backend/routes/points.js
@@ -5,11 +5,11 @@ const Challenge = require('../models/challenge');
 const Reward = require('../models/reward');
 const RewardRequests = require('../collections/reward_requests');
 const knex = require('../knex.js');
-const Bluebird = require('bluebird');
 const moment = require('moment');
 
 const router = express.Router();
 
+const points = {};
 let ptsEarned;
 let ptsUsed;
 let q1;
@@ -17,16 +17,16 @@ let q2;
 let q3;
 let q4;
 let grad;
-const q1Earned = [];
-const q2Earned = [];
-const q3Earned = [];
-const q4Earned = [];
-const totalEarned = [];
-const q1Used = [];
-const q2Used = [];
-const q3Used = [];
-const q4Used = [];
-const totalUsed = [];
+const q1Earned = [0];
+const q2Earned = [0];
+const q3Earned = [0];
+const q4Earned = [0];
+const totalEarned = [0];
+const q1Used = [0];
+const q2Used = [0];
+const q3Used = [0];
+const q4Used = [0];
+const totalUsed = [0];
 
 const getPtsEarned = studentId => new Promise((resolve) => {
   knex('challenge_submissions')
@@ -38,8 +38,7 @@ const getPtsEarned = studentId => new Promise((resolve) => {
     .innerJoin('challenges', 'challenges.id', 'challenge_submissions.challenge_id')
     .then((submissions) => {
       ptsEarned = submissions;
-      // console.log(ptsEarned);
-      resolve('submissions worked');
+      resolve(submissions);
     })
     .catch(err => console.error(err));
 });
@@ -55,7 +54,7 @@ const getPtsUsed = studentId => new Promise((resolve) => {
     .then((requests) => {
       ptsUsed = requests;
       // console.log(ptsUsed);
-      resolve(console.log('requests worked'));
+      resolve(requests);
     })
     .catch(err => console.error(err));
 });
@@ -71,7 +70,6 @@ const getCohortSchedule = studentId => new Promise((resolve) => {
       q3 = dates[0].q3_start_date;
       q4 = dates[0].q4_start_date;
       grad = dates[0].graduation_date;
-      // console.log(q1, q2, q3, q4, grad);
       resolve(dates);
     })
     .catch(err => console.error(err));
@@ -79,16 +77,12 @@ const getCohortSchedule = studentId => new Promise((resolve) => {
 
 const determineQuarter = (time) => {
   if ((moment(time).isAfter(q1)) && (moment(time).isBefore(q2))) {
-    console.log('q1:', time);
     return 'q1';
   } else if ((moment(time).isAfter(q2)) && (moment(time).isBefore(q3))) {
-    console.log('q2:', time);
     return 'q2';
   } else if ((moment(time).isAfter(q3)) && (moment(time).isBefore(q4))) {
-    console.log('q3:', time);
     return 'q3';
   } else if ((moment(time).isAfter(q4)) && (moment(time).isBefore(grad))) {
-    console.log('q4:', time);
     return 'q4';
   }
   return 'This time is not within the program start and end dates';
@@ -96,65 +90,59 @@ const determineQuarter = (time) => {
 
 const distributePts = () => {
   ptsEarned.forEach((submission) => {
-    console.log('Submission:', submission.updated_at);
-    console.log(determineQuarter(submission.updated_at));
+    totalEarned.push(submission.point_value);
     switch (determineQuarter(submission.updated_at)) {
       case 'q1':
-        totalEarned.push(submission.point_value);
         q1Earned.push(submission.point_value);
         break;
       case 'q2':
-        totalEarned.push(submission.point_value);
         q2Earned.push(submission.point_value);
         break;
       case 'q3':
-        totalEarned.push(submission.point_value);
         q3Earned.push(submission.point_value);
         break;
-        console.log('Case q3');
       case 'q4':
-        totalEarned.push(submission.point_value);
         q4Earned.push(submission.point_value);
         break;
       default:
-        console.log('This time is not within the program start and end dates');
+        console.log('The update date is outside of the program start and end dates');
     }
   });
-  console.log('q1 earned:', q1Earned);
-  console.log('q2 earned:', q2Earned);
-  console.log('q3 earned:', q3Earned);
-  console.log('q4 earned:', q4Earned);
-  console.log('total earned:', totalEarned);
   ptsUsed.forEach((request) => {
-    console.log('request:', request.updated_at);
-    console.log(determineQuarter(request.updated_at));
+    totalUsed.push(request.point_cost);
     switch (determineQuarter(request.updated_at)) {
       case 'q1':
-        totalUsed.push(request.point_cost);
         q1Used.push(request.point_cost);
         break;
       case 'q2':
-        totalUsed.push(request.point_cost);
         q2Used.push(request.point_cost);
         break;
       case 'q3':
-        totalUsed.push(request.point_cost);
         q3Used.push(request.point_cost);
         break;
-        console.log('Case q3');
       case 'q4':
-        totalUsed.push(request.point_cost);
         q4Used.push(request.point_cost);
         break;
       default:
-        console.log('This time is not within the program start and end dates');
+        console.log('The update date is outside of the program start and end dates');
     }
   });
-  console.log('q1 Used:', q1Used);
-  console.log('q2 Used:', q2Used);
-  console.log('q3 Used:', q3Used);
-  console.log('q4 Used:', q4Used);
-  console.log('total Used:', totalUsed);
+};
+
+const calculatePts = () => {
+  const points = {};
+  points.totalEarned = totalEarned.reduce((acc, pts) => acc + pts);
+  points.totalUsed = totalUsed.reduce((acc, pts) => acc + pts);
+  points.q1Earned = q1Earned.reduce((acc, pts) => acc + pts);
+  points.q1Used = q1Used.reduce((acc, pts) => acc + pts);
+  points.q2Earned = q2Earned.reduce((acc, pts) => acc + pts);
+  points.q2Used = q2Used.reduce((acc, pts) => acc + pts);
+  points.q3Earned = q3Earned.reduce((acc, pts) => acc + pts);
+  points.q3Used = q3Used.reduce((acc, pts) => acc + pts);
+  points.q4Earned = q4Earned.reduce((acc, pts) => acc + pts);
+  points.q4Used = q4Used.reduce((acc, pts) => acc + pts);
+  points.currentTotal = points.totalEarned - points.totalUsed;
+  return points;
 };
 
 router.get('/points/students/:student_id', (req, res) => {
@@ -162,9 +150,10 @@ router.get('/points/students/:student_id', (req, res) => {
   .then(() => getPtsUsed(req.params.student_id))
   .then(() => getCohortSchedule(req.params.student_id))
   .then(() => {
-    console.log('q4 is here:', q4);
     distributePts();
-  });
+    res.status(200).json(calculatePts());
+  })
+  .catch(err => res.status(500).json(err.message));
 });
 
 // Student.forge({ id: req.params.student_id })
