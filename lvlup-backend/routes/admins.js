2000c37238fce4da2437a2c53d171225db7bd4c9
Thomas Stang <stang.tk@gmail.com>
2017-04-28 05:29:55 -0700

    Refactor admin routes

--- lvlup-backend/routes/admins.js
+++ lvlup-backend/routes/admins.js
@@ -4,17 +4,12 @@ const Admin = require('../models/admin');
 
 const router = express.Router();
 
-
 router.route('/admins/')
   .get((req, res) => {
     Admins.forge()
     .fetch()
-    .then((admins) => {
-      res.json({ admins });
-    })
-    .catch((err) => {
-      res.status(500).json({ error: true, data: { message: err.message } });
-    });
+    .then(admins => res.status(200).json(admins))
+    .catch(err => res.status(500).json(err.message));
   });
 
 router.route('/admins/:id')
@@ -28,27 +23,17 @@ router.route('/admins/:id')
       gravatar_url: req.body.gravatar_url || admin.get('gravatar_url'),
       campus_id: req.body.campus_id || admin.get('campus_id'),
     }))
-    .then((updatedAdmin) => {
-      res.json({ updatedAdmin });
-    })
-    .catch((err) => {
-      res.status(500).json({ error: true, data: { message: err.message } });
-    });
+    .then(updatedAdmin => res.status(200).json(updatedAdmin))
+    .catch(err => res.status(500).json(err.message));
   })
 
   .delete((req, res) => {
     Admin.forge({ id: req.params.id })
     .fetch({ require: true })
-    .then((admin) => {
-      admin.destroy();
-    })
-    .then(() => {
-      res.json({ message: 'Admin successfully deleted' });
-    })
-    .catch((err) => {
-      res.status(500).json({ error: true, data: { message: err.message } });
+    .then((admin) => admin.destroy())
+    .then(() => res.status(200).json({ message: 'Admin successfully deleted' }))
+    .catch((err) => res.status(500).json(err.message);
     });
   });
-
 
 module.exports = router;
