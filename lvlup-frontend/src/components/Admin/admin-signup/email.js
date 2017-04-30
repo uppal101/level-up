44556fd4908d82cc8660bf0c32414daa450ff9f8
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-04-30 10:49:15 -0700

    Add username and campus input for admin sign up

--- lvlup-frontend/src/components/Admin/admin-signup/email.js
+++ lvlup-frontend/src/components/Admin/admin-signup/email.js
@@ -19,6 +19,16 @@ const cohorts = [
   { key: '42', text: 'G42', value: 'g42' },
   { key: '52', text: 'G52', value: 'g52' },
 ];
+const campuses = [
+  { key: 'sf', text: 'San Francisco', value: 'San Francisco' },
+  { key: 'a', text: 'Austin', value: 'Austin' },
+  { key: 'b', text: 'Boulder', value: 'Boulder' },
+  { key: 'dp', text: 'Denver-Platte', value: 'Denver-Platte' },
+  { key: 'dgt', text: 'Denver-Golden Triangle', value: 'Denver-Golden Triangle' },
+  { key: 'ny', text: 'New York', value: 'New York' },
+  { key: 'p', text: 'Phoenix', value: 'Seattle' },
+  { key: 'all', text: 'All campuses', value: 'All Campuses' },
+];
 
 class SignupForm extends Component {
   render() {
@@ -30,6 +40,10 @@ class SignupForm extends Component {
           <Field name="email" component="input" type="email" placeholder="Email" />
         </Form.Field>
         <Form.Field inline>
+          <label>Username</label>
+          <Field name="username" component="input" type="text" placeholder="Username" />
+        </Form.Field>
+        <Form.Field inline>
           <label>Password</label>
           <Field name="password" component="input" type="password" placeholder="Password" />
         </Form.Field>
@@ -38,9 +52,14 @@ class SignupForm extends Component {
           <Field name="confirm-password" component="input" type="password" placeholder="Confirm Password" />
         </Form.Field>
         <Form.Field inline>
+          <label>Campuses</label>
+          <Dropdown placeholder="Campuses" multiple selection options={campuses} />
+          <Field name="campuses" component="dropdown" type="text" placeholder="Select Campuses" />
+        </Form.Field>
+        <Form.Field inline>
           <label>Cohorts</label>
           <Dropdown placeholder="Cohorts" multiple selection options={cohorts} />
-          <Field name="cohorts" component="dropdwon" type="text" placeholder="Select Cohorts" />
+          <Field name="cohorts" component="dropdown" type="text" placeholder="Select Cohorts" />
         </Form.Field>
         <Button content="Sign Up" />
       </Form>
