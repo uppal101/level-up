33ae98f58adf3f866e430d5723d5d4825c0acda7
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-04-30 11:38:54 -0700

    Restructure admin sign up email file to be more readable and set up calls for campus and cohort input

--- lvlup-frontend/src/components/Admin/admin-signup/email.js
+++ lvlup-frontend/src/components/Admin/admin-signup/email.js
@@ -3,7 +3,7 @@ import { Form, Input, Dropdown, Button } from 'semantic-ui-react';
 import { Field, reduxForm } from 'redux-form';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { signup } from '../../../actions/adminsignup';
+import { signup, allCohorts, allCampuses } from '../../../actions/adminsignup';
 import './signupview.css';
 
 function mapDispatchToProps(dispatch) {
@@ -31,35 +31,87 @@ const campuses = [
 ];
 
 class SignupForm extends Component {
+  componentWillMount() {
+    this.props.allCampuses();
+    this.props.allCohorts();
+  }
   render() {
     const { handleSubmit } = this.props;
     return (
       <Form className="forms" onSubmit={handleSubmit(this.props.signup)}>
+
         <Form.Field inline>
           <label>Galvanize Email</label>
-          <Field name="email" component="input" type="email" placeholder="Email" />
+          <Field
+            name="email"
+            component="input"
+            type="email"
+            placeholder="Email"
+          />
         </Form.Field>
+
         <Form.Field inline>
           <label>Username</label>
-          <Field name="username" component="input" type="text" placeholder="Username" />
+          <Field
+            name="username"
+            component="input"
+            type="text"
+            placeholder="Username"
+          />
         </Form.Field>
+
         <Form.Field inline>
           <label>Password</label>
-          <Field name="password" component="input" type="password" placeholder="Password" />
+          <Field
+            name="password"
+            component="input"
+            type="password"
+            placeholder="Password"
+          />
         </Form.Field>
+
         <Form.Field inline>
           <label>Confirm Password</label>
-          <Field name="confirm-password" component="input" type="password" placeholder="Confirm Password" />
+          <Field
+            name="confirm-password"
+            component="input"
+            type="password"
+            placeholder="Confirm Password"
+          />
         </Form.Field>
+
         <Form.Field inline>
           <label>Campuses</label>
-          <Dropdown placeholder="Campuses" multiple selection options={campuses} />
-          <Field name="campuses" component="dropdown" type="text" placeholder="Select Campuses" />
+          <Dropdown
+            placeholder="Campuses" multiple selection options={campusesDropDown(this.props.campuses)}
+            onChange={(event, result) => {
+              const { value } = result;
+              this.props.setCampuses(value);
+            }}
+          />
+          <Field
+            name="campuses"
+            component="dropdown"
+            type="text"
+            placeholder="Select Campuses"
+          />
         </Form.Field>
+
         <Form.Field inline>
           <label>Cohorts</label>
-          <Dropdown placeholder="Cohorts" multiple selection options={cohorts} />
-          <Field name="cohorts" component="dropdown" type="text" placeholder="Select Cohorts" />
+          <Dropdown
+            placeholder="Cohorts" multiple selection options={cohortsDropDown(this.props.cohorts)}
+            onChange={(event, result) => {
+              const { value } = result;
+              this.props.setCohorts(value);
+            }}
+          />
+          <Field
+            name="cohorts"
+            component="dropdown"
+            type="text"
+            placeholder="Select Cohorts"
+          />
         </Form.Field>
         <Button content="Sign Up" />
       </Form>
