36cbc05f1a69e840b6652d8cf862fedebe8f97e1
Daniel Gardner <daniel.marc.gardner@gmail.com>
2017-05-01 10:15:31 -0700

    Add logout routes.

---
+++ lvlup-backend/routes/logout.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
+const session = require('express-session');
+const router = express.Router();
+
+router.route('/student/logout')
+  .get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+router.route('admin/logout')
+  .get((req, res) => {
+    res.cookie('authToken', null);
+  });
