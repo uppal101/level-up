ef9d54bb2673a55642bad5da90fc5155e2ada22d
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-05-01 11:35:37 -0700

    Show working front-end validation on password for admin login

--- lvlup-frontend/src/components/Admin/admin-login/email.js
+++ lvlup-frontend/src/components/Admin/admin-login/email.js
@@ -16,7 +16,7 @@ function mapStateToProps(dispatch) {
 }
 
 const required = value => value ? undefined : 'Required';
-const minValue = min => value => value && value < min ? `Must be at least ${min} characters` : undefined;
+const minValue = min => value => value && value.length < min ? `Must be at least ${min} characters or more` : undefined;
 const minValue7 = minValue(7);
 const email = value =>
   value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
