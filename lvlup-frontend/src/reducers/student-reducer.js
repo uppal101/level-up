f62730e4f22f2df06d3bc31d19380d501b04d352
Thomas Stang <stang.tk@gmail.com>
2017-05-01 17:08:56 -0700

    Resolve point doubling on refresh

--- lvlup-frontend/src/reducers/student-reducer.js
+++ lvlup-frontend/src/reducers/student-reducer.js
@@ -11,7 +11,7 @@ export const loginInfo = (state = {}, action) => {
 export const studentPointsAndCampus = (state = {}, action) => {
   switch (action.type) {
     case CONST.POINTS_COHORT_FULFILLED:
-      return action.payload;
+      return Object.assign({}, action.payload);
     default:
       return state;
   }
