23c9f5314522c22e2e3ec48bbffcc5e428bbfaf7
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-06-06 15:53:36 -0700

    Attempt to start testing async actions

---
+++ lvlup-frontend/test/add-challenge-actions.test.js
@@ -0,0 +1,11 @@
+// import * as actions from '../src/actions/add-challenge';
+// import * as types from '../src/constants/constants';
+import axios from 'axios';
+
+describe('actions', () => {
+  it('should allow an admin to add a challenge', () => {
+    const expectedAction = {
+
+    };
+  });
+});
