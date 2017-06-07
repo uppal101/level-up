8c16a64c19ce061be8a00c20f23c0ec1c19f9109
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-06-06 18:06:28 -0700

    Fix bug for individual challenges

--- lvlup-frontend/test/add-challenge-actions.test.js
+++ lvlup-frontend/test/add-challenge-actions.test.js
@@ -1,11 +1,28 @@
-// import * as actions from '../src/actions/add-challenge';
-// import * as types from '../src/constants/constants';
-import axios from 'axios';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import nock from 'nock';
+import expect from 'expect';
+import * as actions from '../src/actions/add-challenge';
+import * as types from '../src/constants/constants';
 
-describe('actions', () => {
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('async actions', () => {
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
   it('should allow an admin to add a challenge', () => {
-    const expectedAction = {
+    nock()
+    .post('/api/challenges')
+    .reply(200, { body: })
 
-    };
+    const expectedActions = [
+      { type: types.RESET_ADD_CHALLENGE },
+      { type: types.ADD_CHALLENGE_FULFILLED, body: {}}
+    ]
+
+    const store = mockStore()
   });
 });
