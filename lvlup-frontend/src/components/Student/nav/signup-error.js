8b305a9b27f1b2fe317e8f6b0f92b18bb63aac0c
Thomas Stang <stang.tk@gmail.com>
2017-06-06 18:03:39 -0700

    Fix styling for error messages

--- lvlup-frontend/src/components/Student/nav/signup-error.js
+++ lvlup-frontend/src/components/Student/nav/signup-error.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import { Message, Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import './sidenav-styles.css';
 
 class SignUpError extends Component {
   render() {
     return (
-      <Message
-        color="red"
-        header="GitHub Error!"
-      >
-        <p>GitHub OAuth Error! Please return home and try again. If problem persists please contact: lvlupteam@lvlup.tech</p>
-        <Link to={'/'}><Button basic color="orange"> Click Here to Go Home</Button></Link>
-      </Message>
+      <div className="oauth-error">
+        <Message
+          color="red"
+          header="GitHub Error!"
+        >
+          <p>GitHub OAuth Error! Please return home and try again. If problem persists please contact: lvlupteam@lvlup.tech</p>
+          <Link to={'/'}><Button basic color="orange"> Click Here to Go Home</Button></Link>
+        </Message>
+      </div>
     );
   }
 }
