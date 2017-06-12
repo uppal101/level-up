16a9f9570752dee95588ab89f70ce4ba4c466c12
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-06-12 15:30:36 -0700

    Start some student reducer tests

--- lvlup-frontend/test/student-reducer.test.js
+++ lvlup-frontend/test/student-reducer.test.js
@@ -1,9 +1,15 @@
 import { loginInfo, studentPointsAndCampus, selectedReward, submissions, challenges, rewards, requests, submittedChallenge, selectedChallenge, requestedReward } from '../src/reducers/student-reducer';
+import * as CONST from '../src/constants/constants';
 
 
 describe('student login reducer', () => {
   it('should return the initial state', () => {
     expect(loginInfo(undefined, {})).toEqual({ status: false });
+  });
+  it('should return a new state with the submitted challenge', () => {
+    const prevState = { status: false };
+    const nextState = loginInfo(prevState, { type: CONST.STUDENT_LOGIN_FULFILLED, id: 2, email: 'sanjeet.uppal92@gmail.com' });
+    expect(nextState).toEqual({ status: true });
   });
 });
 
@@ -47,16 +53,31 @@ describe('submitted challenge reducer', () => {
   it('should return the initial state', () => {
     expect(submittedChallenge(undefined, {})).toEqual({ fulfilled: false });
   });
+  it('should return a new state with the submitted challenge', () => {
+    const prevState = { fulfilled: false };
+    const nextState = submittedChallenge(prevState, { type: CONST.CHALLENGE_SUBMISSION_FULFILLED, student_id: 2, challenge_id: 11, cohort_id: 1, description: 'Introduced Ryan to QA Automation in government division for air quality', category_id: 3 });
+    expect(nextState).toEqual({ fulfilled: true });
+  });
 });
 
 describe('selected challenge reducer', () => {
   it('should return the initial state', () => {
     expect(selectedChallenge(undefined, {})).toEqual({});
   });
+  it('should return a new state with the submitted challenge', () => {
+    const prevState = { fulfilled: false };
+    const nextState = submittedChallenge(prevState, { type: CONST.CHALLENGE_SUBMISSION_FULFILLED, student_id: 2, challenge_id: 11, cohort_id: 1, description: 'Introduced Ryan to QA Automation in government division for air quality', category_id: 3 });
+    expect(nextState).toEqual({ fulfilled: true });
+  });
 });
 
 describe('requested reward reducer', () => {
   it('should return the initial state', () => {
     expect(requestedReward(undefined, {})).toEqual({ fulfilled: false });
+  });
+  it('should return a new state with the requested reward', () => {
+    const prevState = { fulfilled: false };
+    const nextState = requestedReward(prevState, { type: CONST.REWARD_REQUEST_FULFILLED, student_id: 2, reward_id: 11, cohort_id: 1, notes: 'envelope please', category_id: 4 });
+    expect(nextState).toEqual({ fulfilled: true });
   });
 });
