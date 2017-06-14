1838f10134a62fafe805c5fc5654852aa566a1a1
Daniel Gardner <daniel.marc.gardner@gmail.com>
2017-06-14 10:56:17 -0700

    Begin refactor of Add Reward to pure components.

--- lvlup-frontend/src/components/Admin/rewards/add-reward/add-reward-main.js
+++ lvlup-frontend/src/components/Admin/rewards/add-reward/add-reward-main.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import renderIf from 'render-if';
-import AddRewardForm from './add-reward-form';
-import AddRewardCompleted from './add-reward-completed';
+import AddRewardForm from './add-reward-form-main';
+import AddRewardCompleted from './add-reward-completed-main';
 
 const mapStateToProps = state => ({
   addStatus: state.addedReward,
