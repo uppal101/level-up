de3fbbe29f59143489850d2c7d48c78118b26952
Thomas Stang <stang.tk@gmail.com>
2017-06-14 11:36:12 -0700

    Refactor admin add challenge components

--- lvlup-frontend/src/components/Admin/admin-common/render-challenges.js
+++ lvlup-frontend/src/components/Admin/admin-common/render-challenges.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Icon, Table } from 'semantic-ui-react';
+import { Icon, Table, List } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 const renderChallenges = props => props.challenges.challenges.filter(challenges => challenges.active === 'Active').map(item => (
@@ -7,16 +7,7 @@ const renderChallenges = props => props.challenges.challenges.filter(challenges 
     <Table.Cell>{item.name}</Table.Cell>
     <Table.Cell>{item.category.category}</Table.Cell>
     <Table.Cell>{item.description}</Table.Cell>
-    <Table.Cell>
-      {item.requirements_1 ? <List bulleted>
-        <List.Item>{item.requirements_1}</List.Item>
-        {item.requirements_2 ? <List.Item>{item.requirements_2}</List.Item> : null}
-        {item.requirements_3 ? <List.Item>{item.requirements_3}</List.Item> : null}
-        {item.requirements_4 ? <List.Item>{item.requirements_4}</List.Item> : null}
-        {item.requirements_5 ? <List.Item>{item.requirements_5}</List.Item> : null}
-      </List> : 'No requirements!'}
-    </Table.Cell>
-    <Table.Cell textAlign="center" onClick={() => this.props.selectChallenge(item)}>
+    <Table.Cell textAlign="center" onClick={() => props.selectChallenge(item)}>
       <Link to={`/admin/challenge-edit/${item.id}`}>
         <Icon color="orange" name="pencil" />
       </Link>
