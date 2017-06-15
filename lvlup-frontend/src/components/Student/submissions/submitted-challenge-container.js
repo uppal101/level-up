3e32e9f69d89d320885b65bf0b3673c1cf0fc707
Daniel Gardner <daniel.marc.gardner@gmail.com>
2017-06-15 16:36:49 -0700

    Fix front end bugs.

--- lvlup-frontend/src/components/Student/submissions/submitted-challenge-container.js
+++ lvlup-frontend/src/components/Student/submissions/submitted-challenge-container.js
@@ -1,27 +1,13 @@
-import { compose, lifecycle } from 'recompose';
+import { compose } from 'recompose';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { reduxForm } from 'redux-form';
-import uploadcare from 'uploadcare-widget';
-import ChallengeSubmissionForm from './submission-form';
-import { challengeSubmission } from '../../../actions/student-challenges-actions';
+import SubmissionCompleted from './submission-completed';
 
 const mapStateToProps = state => ({
+  submission: state.submittedChallenge,
   loginInfo: state.loginInfo,
   selectedChallenge: state.selectedChallenge,
 });
 
-const mapDispatchToProps = dispatch => bindActionCreators({ challengeSubmission }, dispatch);
+const connectToStore = connect(mapStateToProps);
 
-const connectToStore = connect(mapStateToProps, mapDispatchToProps);
-
-const onDidMount = lifecycle({
-  componentDidMount() {
-    uploadcare.start({
-      publicKey: '1c7b669f9c1e6b59ad80',
-      tabs: 'all',
-    });
-  },
-});
-
-export default compose(connectToStore, onDidMount)(reduxForm({ form: 'submission' })(ChallengeSubmissionForm));
+export default compose(connectToStore)(SubmissionCompleted);
