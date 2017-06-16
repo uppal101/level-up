5ac26bb8e1f6bf441a96195a3ac49df7a8f24c61
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-06-15 21:10:05 -0700

    Change reward reducers to match  new file structure

--- lvlup-frontend/test/reducers/reward-reducer.test.js
+++ lvlup-frontend/test/reducers/reward-reducer.test.js
@@ -1,5 +1,5 @@
-import { approveSelectedReward, denySelectedReward, inactiveReward } from '../src/reducers/request-reducer';
-import * as CONST from '../src/constants/constants';
+import { approveSelectedReward, denySelectedReward, inactiveReward, addedReward, editedReward, requestedReward, requests, rewards, selectedReward } from '../../src/reducers/reward-reducers';
+import * as CONST from '../../src/constants/constants';
 
 describe('approve reward reducer', () => {
   it('should return the initial state', () => {
@@ -32,5 +32,79 @@ describe('inactive reward reducer', () => {
     const prevState = { status: 'Pending' };
     const nextState = inactiveReward(prevState, { type: CONST.INACTIVE_REWARD_FULFILLED, name: 'gSwag', point_cost: 50, description: 'Galvanize apparel', campus_id: 1, category_id: 4 });
     expect(nextState).toEqual({ active: false });
+  });
+});
+
+describe('selected reward reducer', () => {
+  it('should return the initial state', () => {
+    expect(selectedReward(undefined, {})).toEqual({});
+  });
+
+  it('should return a new state with the selected reward', () => {
+    const prevState = {};
+    const nextState = selectedReward(prevState, { type: CONST.SELECTED_REWARD, reward: { test: 2 } });
+    expect(nextState).toEqual({ test: 2 });
+  });
+});
+
+
+describe('rewards reducer', () => {
+  it('should return the initial state', () => {
+    expect(rewards(undefined, {})).toEqual({ rewards: [] });
+  });
+
+  it('should return a new state with the reward', () => {
+    const prevState = { rewards: [] };
+    const nextState = rewards(prevState, { type: CONST.REWARDS_CAMPUS_FULFILLED, payload: [{ test: 8 }] });
+
+    expect(nextState).toEqual({ rewards: [{ test: 8 }] });
+  });
+});
+
+describe('requests reducer', () => {
+  it('should return the initial state', () => {
+    expect(requests(undefined, {})).toEqual({ requests: [] });
+  });
+
+  it('should return a new state with the request', () => {
+    const prevState = { requests: [] };
+    const nextState = requests(prevState, { type: CONST.REQUESTS_FULFILLED, payload: [{ test: 4 }] });
+    expect(nextState).toEqual({ requests: [{ test: 4 }] });
+  });
+});
+
+
+describe('requested reward reducer', () => {
+  it('should return the initial state', () => {
+    expect(requestedReward(undefined, {})).toEqual({ fulfilled: false });
+  });
+
+  it('should return a new state with the requested reward', () => {
+    const prevState = { fulfilled: false };
+    const nextState = requestedReward(prevState, { type: CONST.REWARD_REQUEST_FULFILLED, student_id: 2, reward_id: 11, cohort_id: 1, notes: 'envelope please', category_id: 4 });
+    expect(nextState).toEqual({ fulfilled: true });
+  });
+});
+
+describe('add reward reducer', () => {
+  it('should return the initial state when no action is passed in', () => {
+    expect(addedReward(undefined, {})).toEqual({ fulfilled: false });
+  });
+
+  it('should return a new state with the added reward', () => {
+    const prevState = { fulfilled: false };
+    const nextState = addedReward(prevState, { type: CONST.ADD_REWARD_FULFILLED, name: 'gSwag', point_cost: 50, description: 'Galvanize apparel', campus_id: 1, category_id: 4 });
+    expect(nextState).toEqual({ fulfilled: true });
+  });
+});
+
+describe('edit reward reducer', () => {
+  it('should return the initial state', () => {
+    expect(editedReward(undefined, {})).toEqual({ fulfilled: false });
+  });
+  it('should return a new state with the edited reward', () => {
+    const prevState = { fulfilled: false };
+    const nextState = editedReward(prevState, { type: CONST.EDIT_REWARD_FULFILLED, name: 'gSwag', point_cost: 50, description: 'Galvanize hat', campus_id: 1, category_id: 4 });
+    expect(nextState).toEqual({ fulfilled: true });
   });
 });
--- lvlup-frontend/test/student-reducer.test.js
+++ lvlup-frontend/test/student-reducer.test.js
@@ -1,4 +1,4 @@
-import { studentLoginInfo, studentPointsAndCampus, selectedReward, rewards, requests, requestedReward } from '../src/reducers/student-reducer';
+import { studentLoginInfo, studentPointsAndCampus } from '../src/reducers/student-reducer';
 import * as CONST from '../src/constants/constants';
 
 
@@ -35,56 +35,5 @@ describe('student points and campus reducer', () => {
     const prevState = {};
     const nextState = studentPointsAndCampus(prevState, { type: CONST.POINTS_COHORT_FULFILLED, payload: { test: 1 } });
     expect(nextState).toEqual({ test: 1 });
-  });
-});
-
-describe('selected reward reducer', () => {
-  it('should return the initial state', () => {
-    expect(selectedReward(undefined, {})).toEqual({});
-  });
-
-  it('should return a new state with the selected reward', () => {
-    const prevState = {};
-    const nextState = selectedReward(prevState, { type: CONST.SELECTED_REWARD, reward: { test: 2 } });
-    expect(nextState).toEqual({ test: 2 });
-  });
-});
-
-
-describe('rewards reducer', () => {
-  it('should return the initial state', () => {
-    expect(rewards(undefined, {})).toEqual({ rewards: [] });
-  });
-
-  it('should return a new state with the reward', () => {
-    const prevState = { rewards: [] };
-    const nextState = rewards(prevState, { type: CONST.REWARDS_CAMPUS_FULFILLED, payload: [{ test: 8 }] });
-
-    expect(nextState).toEqual({ rewards: [{ test: 8 }] });
-  });
-});
-
-describe('requests reducer', () => {
-  it('should return the initial state', () => {
-    expect(requests(undefined, {})).toEqual({ requests: [] });
-  });
-
-  it('should return a new state with the request', () => {
-    const prevState = { requests: [] };
-    const nextState = requests(prevState, { type: CONST.REQUESTS_FULFILLED, payload: [{ test: 4 }] });
-    expect(nextState).toEqual({ requests: [{ test: 4 }] });
-  });
-});
-
-
-describe('requested reward reducer', () => {
-  it('should return the initial state', () => {
-    expect(requestedReward(undefined, {})).toEqual({ fulfilled: false });
-  });
-
-  it('should return a new state with the requested reward', () => {
-    const prevState = { fulfilled: false };
-    const nextState = requestedReward(prevState, { type: CONST.REWARD_REQUEST_FULFILLED, student_id: 2, reward_id: 11, cohort_id: 1, notes: 'envelope please', category_id: 4 });
-    expect(nextState).toEqual({ fulfilled: true });
   });
 });
