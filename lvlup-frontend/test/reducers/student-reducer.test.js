4042710f05dafa2d4b118437fdc665c03cb376a9
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-06-16 12:26:29 -0700

    Start testing rejected cases

--- lvlup-frontend/test/reducers/student-reducer.test.js
+++ lvlup-frontend/test/reducers/student-reducer.test.js
@@ -13,10 +13,22 @@ describe('student login reducer', () => {
     expect(nextState).toEqual({ status: true });
   });
 
+  it('should return old state when sent incorrect input for login', () => {
+    const prevState = { status: false };
+    const nextState = studentLoginInfo(prevState, { type: CONST.STUDENT_LOGIN_REJECTED, reject : [ test: 1]});
+    expect(nextState).toEqual({ status: false, error: 'An Error Occured During Auth from GitHub. Please Try Again' });
+  });
+
   it('should return a new state with the signed in user', () => {
     const prevState = { status: false };
     const nextState = studentLoginInfo(prevState, { type: CONST.STUDENT_SIGNUP_FULFILLED, name: 'Sanjeet Uppal', email: 'uppal101@mail.chapman.edu', cohort_id: 1, username: 'uppal101' });
     expect(nextState).toEqual({ status: true });
+  });
+
+  it('should return old state when sent incorrect input for sign up', () => {
+    const prevState = { status: false };
+    const nextState = studentLoginInfo(prevState, { type: CONST.STUDENT_SIGNUP_REJECTED, reject : [ test: 1]});
+    expect(nextState).toEqual({ status: false, error: 'Server Error - Please Try Again' });
   });
 
   it('should return a new state with a logged out user', () => {
@@ -35,5 +47,11 @@ describe('student points and campus reducer', () => {
     const prevState = {};
     const nextState = studentPointsAndCampus(prevState, { type: CONST.POINTS_COHORT_FULFILLED, payload: { test: 1 } });
     expect(nextState).toEqual({ test: 1 });
+  });
+
+  it('should return old state when sent incorrect input for student points and campus', () => {
+    const prevState = {};
+    const nextState = studentPointsAndCampus(prevState, { type: CONST.STUDENT_SIGNUP_REJECTED, reject : [ test: 1]});
+    expect(nextState).toEqual({});
   });
 });
