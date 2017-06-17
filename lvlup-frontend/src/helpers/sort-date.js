f1c394d30433a0ac4af427250ec8559e1e2b6520
Thomas Stang <stang.tk@gmail.com>
2017-06-17 14:34:22 -0700

    Fix decrement error

--- lvlup-frontend/src/helpers/sort-date.js
+++ lvlup-frontend/src/helpers/sort-date.js
@@ -2,13 +2,13 @@ import moment from 'moment';
 
 const merge = (left, right) => {
   const result = [];
-  const l = 0;
-  const r = 0;
+  let l = 0;
+  let r = 0;
   while (l < left.length && r < right.length) {
     if (moment(left[l].created_at).isBefore(moment(right[r].created_at))) {
-      result.push(left[l + 1]);
+      result.push(left[l++]);
     } else {
-      result.push(right[r + 1]);
+      result.push(right[r++]);
     }
   }
   return result.concat(left.slice(l), right.slice(r));
@@ -27,7 +27,7 @@ export const quickSort = (arr) => {
   const pivot = [arr[arr.length - 1]];
   const leftArr = [];
   const rightArr = [];
-  for (let i = 0; i < arr.length - 1; i + 1) {
+  for (let i = 0; i < arr.length - 1; i++) {
     if (moment(arr[i].created_at).isAfter(moment(pivot[0].created_at))) {
       rightArr.push(arr[i]);
     } else {
